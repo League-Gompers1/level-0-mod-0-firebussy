00bd65ba9b29528b210eab4b1393a009d5cf411a
firebussy <eniola.odunuga@gompersstudent.org>
2017-05-15 14:52:04 -0700

    Add files via upload

---
+++ For_loop_Gauntlet.java
@@ -0,0 +1,15 @@
+package for_loops;
+
+public class For_loop_Gauntlet {
+public static void main(String[] args) {
+	for(int i= 0;i<=100;i=i+1){
+		System.out.println(i);
+}
+	for(int i= 100;i>=0;i=i-1){
+		System.out.println(i);
+		}
+	for(int i= 2;i<=0;i=i+2){
+		System.out.println(i);
+	}
+
+}}
