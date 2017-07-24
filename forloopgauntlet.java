374c57e8bd5f565baf3c80bfca1de309037d5304
firebussy <eniola.odunuga@gompersstudent.org>
2017-07-24 14:59:01 -0700

    Add files via upload

---
+++ forloopgauntlet.java
@@ -0,0 +1,36 @@
+
+public class forloopgauntlet {
+	public static void main(String[] args) {
+	for( int i=0;i<101;i++){
+			System.out.println(i);
+	}
+		for( int i=100;i>=0;i--){
+			System.out.println(i);
+	
+		}
+		for( int i=0;i<101;i+=2){
+			System.out.println(i);
+}
+		for( int i=1;i<=99;i+=2){
+			System.out.println(i);
+}
+		for( int i=0;i<=500;i++){
+			System.out.println(i);
+			if(i%2==0){
+				System.out.println("even");
+			}
+			else{
+				System.out.println("odd");
+			}
+			
+			}
+		
+	for( int i=7;i<=777;i+=7){
+			System.out.println(i);}
+	
+	for( int i=2005;i<=2017;i-=1){
+		System.out.println(i);
+		System.out.println("I was ");
+		System.out.println(2017-i);
+	}}}
+	
